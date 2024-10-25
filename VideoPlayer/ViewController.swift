2323a21eb66fe87e1042bce8e7ca392444fb4656
fiesematente <mosesfinimento@googlemail.com>
2024-10-25 12:21:45 +0200

    single video is playable

--- VideoPlayer/ViewController.swift
+++ VideoPlayer/ViewController.swift
@@ -6,18 +6,32 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var playButton: UIButton!
     
+    let data = "12225583_3840_2160_30fps"
     override func viewDidLoad() {
         super.viewDidLoad()
         playButton.layer.cornerRadius = 20
     }
 
     @IBAction func playButtonTapped(_ sender: UIButton) {
-        print("playButtonTapped")
+        //print("playButtonTapped")
+        
+        let url = Bundle.main.url(forResource: data, withExtension: "mp4")
+        guard let videoURL = url else { return }
+        
+        let player = AVPlayer(url: videoURL)
+        let vc = AVPlayerViewController()
+        
+        vc.player = player
+        
+        present(vc, animated: true){
+            vc.player?.play()
+        }
     }
     
 
